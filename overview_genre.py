da655c6319e6856af217fcb3a10000cbf54732e4
zelotype <60070002@kmitl.ac.th>
2017-11-21 09:26:21 +0700

    update code2

--- overview_genre.py
+++ overview_genre.py
@@ -1,21 +1,28 @@
-"""..."""
+"""
+PSIT Data Analysis Project
+Chart: Overview of Genre Types
+"""
 
-import numpy, pandas, pygal
+import pandas, numpy, pygal
 from project_module import project
 
 def main():
-    """main controller"""
+    """ Main function """
     data_frame = pandas.read_csv('videoGameSales.csv')
     create_chart(data_frame)
 
 def create_chart(data_frame):
-    """for create chart"""
+    """ For creating chart """
     data = numpy.array(data_frame.groupby('Genre', as_index=False).count()[['Genre', 'Rank']]).tolist()
+
     chart = pygal.Pie()
+
     for i in data:
         chart.add(i[0], i[1])
-    chart.title = 'Genres of Video Games'
+
+    chart.title = 'Video Games Amount by Genre'
     chart.legend_at_bottom = True
+    chart.legend_box_size = 12
     chart.render_to_file('overview_genre.svg')
 
 main()
--- overview_platform.py
+++ overview_platform.py
@@ -1,24 +1,29 @@
-"""..."""
+"""
+PSIT Data Analysis Project
+Chart: Overview of Platform Types
+"""
 
-import pandas as pandas
-import numpy as numpy
-import pygal as pygal
-
+import pandas, numpy, pygal
 from project_module import project
 
 def main():
-    """main controller"""
-    data_frame = pandas.read_csv('videoGameSales.csv')
+    """ Main function """
+    data_frame = project.platform_convert_df(pandas.read_csv('videoGameSales.csv'))
     create_chart(data_frame)
 
 def create_chart(data_frame):
-    """for create chart"""
-    df_convert = project.platform_convert_df(data_frame)
-    data = numpy.array(df_convert.groupby('Platform', as_index=False).count()[['Platform', 'Rank']]).tolist()
+    """ For creating chart """
+    data = numpy.array(data_frame.groupby('Platform', as_index=False).count()[['Platform', 'Rank']]).tolist()
+
     chart = pygal.Pie()
+
     for i in data:
         chart.add(i[0], i[1])
-    chart.title = 'Platform Types of Video Games'
+
+    chart.title = 'Video Games Amount by Platform Type'
     chart.legend_at_bottom = True
-    chart.render_to_file('platform.svg')
+    chart.legend_at_bottom_columns = 3
+    chart.legend_box_size = 12
+    chart.render_to_file('overview_platform.svg')
+
 main()
